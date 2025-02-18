2b3f9f895df284ab689a5ed4708767d6d2369a62
Tetroit <shichinov.ivan@gmail.com>
2025-02-18 22:23:18 +0100

    cleanup

--- premake5.lua
+++ premake5.lua
@@ -10,7 +10,6 @@ project "GLFW"
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/context.c",
-        "src/egl_context.c",
 		"src/init.c",
 		"src/input.c",
         "src/internal.h",
