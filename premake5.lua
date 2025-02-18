6a2d40a756027d056905ccc80917891b3a23942b
Tetroit <shichinov.ivan@gmail.com>
2025-02-18 21:26:48 +0100

    premake update

--- premake5.lua
+++ premake5.lua
@@ -9,15 +9,26 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
 		"src/context.c",
+        "src/egl_context.c",
 		"src/init.c",
 		"src/input.c",
+        "src/internal.h",
+        "src/mappings.h",
 		"src/monitor.c",
+        "src/platform.h",
+        "src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+
+        "src/null_platform.h",
+        "src/null_joystick.h",
+        "src/null_init.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c",
 	}
-    
+
     filter "system:windows"
 		systemversion "latest"
 		staticruntime "On"
@@ -25,20 +36,23 @@ project "GLFW"
 		files
 		{
 			"src/win32_init.c",
+			"src/win32_module.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
+            "src/win32_time.h",
 			"src/win32_time.c",
+            "src/win32_thread.h",
 			"src/win32_thread.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
 			"src/egl_context.c",
-			"src/osmesa_context.c"
+			"src/osmesa_context.c",
 		}
 
 		defines 
 		{ 
 			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
+			"_CRT_SECURE_NO_WARNINGS",
 		}
 
 	filter "configurations:Debug"
